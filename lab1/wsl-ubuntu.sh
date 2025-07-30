70b0174a15cd4da6c2875845585a39159c274a3d
Aayush Mishra <manofganges@gmail.com>
2025-07-31 02:05:54 +0530

    Update wsl-ubuntu.sh

--- lab1/wsl-ubuntu.sh
+++ lab1/wsl-ubuntu.sh
@@ -1,1 +1,14 @@
+#step01-install wsl in your windows
+wsl --install
 
+#step02-install ubuntu via wsl in your terminal/command-line
+wsl --install -d Ubuntu-22.04
+
+#step03-launch ubuntu and set-up your username and password for the linux environment
+wsl -d Ubuntu-22.04
+
+#update and upgrade packages
+sudo apt update && sudo apt upgrade -y
+
+#check the installed version in your ubuntu
+lsb_release -a

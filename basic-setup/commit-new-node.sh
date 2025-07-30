8c2827b079a6b26d2c9e1c1d122eb9092808625f
Aayush Mishra <manofganges@gmail.com>
2025-07-30 23:30:29 +0530

    Create commit-new-node.sh

---
+++ basic-setup/commit-new-node.sh
@@ -0,0 +1,5 @@
+#creates a python node named `my_node` in the folder/directory named `my_py_node`
+my_py_node/my_py_node/my_node.py
+
+#creates a c++ node named `my_node` in the folder/directory named `my_cpp_node`
+my_cpp_node/src/my_node.cpp

9d3e1eac89f4b86a7be2d69b9a74092caae6ab8a
Aayush Mishra <manofganges@gmail.com>
2025-08-02 14:38:50 +0530

    edit in node

--- lab2/simple.py
+++ lab2/simple.py
@@ -2,14 +2,13 @@
 
 import rclpy
 from rclpy.node import Node
-
 class MyNode(Node):
     def __init__(self):
         super().__init__('sample')
         self.create_timer(0.2, self.timer_callback)
 
     def timer_callback(self):
-        self.get_logger().info("I am in ROS Lab")
+        self.get_logger().info("[Enter_Your_Name] is in ROS Lab")
 
 def main(args=None):
     rclpy.init(args=args)
